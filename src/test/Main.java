7724aea52b58c52abd9f41b1c9991e09da1e731f
runov-daniil <runov-daniil@yandex.ru>
2016-12-05 21:55:56 +0200

    Реализация общения через консоль

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,8 +1,30 @@
 package test;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hello World");
+        int cmd = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Выберите команду:");
+        System.out.println("1: Калькулятор;");
+        System.out.println("2: Игра.");
+        cmd = scanner.nextInt();
+        if((cmd == 1)){
+            calculator();
+        }else if(cmd == 2){
+            game();
+        }else{
+            System.out.println("Такой команды нет!");
+        }
+    }
+
+    private static void calculator(){
+
+    }
+
+    private static void game(){
+
     }
 }
